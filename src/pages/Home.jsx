5960be8415d64e8c62d629563c50f9cdb61c3eec
YashGoyal06 <yash.goyal06@proton.me>
2026-05-05 14:43:33 +0530

    feat: add 3D scroll showcase section with ContainerScroll animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Layout from '../components/layout/Layout';
 import Hero from '../components/sections/Hero';
+import ScrollShowcase from '../components/sections/ScrollShowcase';
 import ServicesGrid from '../components/sections/ServicesGrid';
 import FeaturedProjects from '../components/sections/FeaturedProjects';
 import WhyUs from '../components/sections/WhyUs';
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <Layout>
       <Hero />
+      <ScrollShowcase />
       <ServicesGrid />
       <FeaturedProjects />
       <WhyUs />
